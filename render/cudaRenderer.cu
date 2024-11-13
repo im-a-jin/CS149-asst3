6a3b2012a69ff392adfafb1c5b33ed8319ff239c
Matthew Jin <mjin.code@gmail.com>
2024-11-13 00:07:02 -0800

    Revert "Add totalCircleBlock calculation optimization"
    This reverts commit 08b14f0c09af01dd5b69fd96703fbcaa35985920.

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -476,12 +476,8 @@ __global__ void kernelRenderCircles() {
     float2 pixelCenterNorm = make_float2(invWidth * (static_cast<float>(pixelX) + 0.5f),
                                          invHeight * (static_cast<float>(pixelY) + 0.5f));
 
-    if (linearThreadIndex == 0) {
-        prefixSumScratch[0] = prefixSumInput[SCAN_BLOCK_DIM-1] + prefixSumOutput[SCAN_BLOCK_DIM-1];
-    }
-    __syncthreads();
+    uint totalCirclesBlock = prefixSumInput[SCAN_BLOCK_DIM-1] + prefixSumOutput[SCAN_BLOCK_DIM-1];
 
-    uint totalCirclesBlock = prefixSumScratch[0];
     if (totalCirclesBlock == 0)
         return;
 
